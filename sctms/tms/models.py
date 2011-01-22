a89694d93088368f2bf636eb01823ddd084c9b7a
architech <architech@centrum.cz>
2011-01-22 20:33:59 +0100

    replay model/db fix

--- sctms/tms/models.py
+++ sctms/tms/models.py
@@ -757,7 +757,7 @@ class Replay(CacheNotifierModel):
     match = models.ForeignKey(Match)
     map = models.ForeignKey(Map, null=True, blank=True)
     description = models.TextField(blank=True)
-    uploaded_by = models.ForeignKey(User)
+    uploaded_by = models.ForeignKey(User, null=True, blank=True)
 
     class Meta:
         verbose_name = _(u'Replay')
