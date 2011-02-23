80a2bf84c2fc45cc12e632fea8bbb47b05a36cc3
martymarty <martin.rendl@gmail.com>
2011-02-23 22:57:40 +0100

    tak zase zpatky woe
    Signed-off-by: martymarty <martin.rendl@gmail.com>

--- nslcms/forms/templatetags/form_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.inclusion_tag('forms/field.html')
-def field(field, extra_class=''):
-    widget_class = field.field.widget.__class__.__name__
-    return locals()
--- nslcms/pages/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('pages.views',
-    url(r'^(?P<slug>[\w_-]+)/$', 'page', name='page'),
-)
