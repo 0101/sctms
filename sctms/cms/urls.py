ec92b1196c45faa32022c494de33eb60e11ab453
martymarty <martin.rendl@gmail.com>
2011-03-07 21:24:17 +0100

    varlat v pytli redukce zbytecnejm importum
    Signed-off-by: martymarty <martin.rendl@gmail.com>

--- sctms/cms/urls.py
+++ sctms/cms/urls.py
@@ -1,18 +1,6 @@
-from django.conf import settings
 from django.conf.urls.defaults import *
-from django.contrib import admin
-from django.views.generic.simple import direct_to_template, redirect_to
-
-admin.autodiscover()
 
 urlpatterns = patterns('cms.views',
     url(r'^$', 'index', name='index'),
     url(r'^(?P<slug>[\w_-]+)/detail/$', 'detail', name='detail'),
-    )
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        # serve static files only for development
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-         {'document_root': settings.MEDIA_ROOT}),
     )
--- sctms/cms/views.py
+++ sctms/cms/views.py
@@ -1,25 +1,9 @@
-from datetime import datetime
-from random import randint
-
 from django.conf.urls.defaults import patterns, url
 from django.contrib import auth
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import Group, User
-from django.contrib.sessions.backends.db import SessionStore
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.urlresolvers import reverse
-from django.db.models import Q
-from django.db import transaction
-from django.http import HttpResponseRedirect, HttpResponse, HttpResponseBadRequest
-from django.shortcuts import get_object_or_404, render_to_response
-from django.template import RequestContext
+from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
-from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.simple import direct_to_template
-
-from nyxauth import NyxAuth
 
 from cms.models import Comment, BlogEntry
 
