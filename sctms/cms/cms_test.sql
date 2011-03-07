34dd19008861276040c76f7222e2a9e0d57c8be9
martymarty <martin.rendl@gmail.com>
2011-03-07 20:16:24 +0100

    tak snad release ty bety teda :)
    Signed-off-by: martymarty <martin.rendl@gmail.com>

--- sctms/cms/cms_test.sql
+++ sctms/cms/cms_test.sql
@@ -3,36 +3,13 @@
 # Server version:               5.5.9
 # Server OS:                    Win32
 # HeidiSQL version:             6.0.0.3603
-# Date/time:                    2011-03-07 20:10:17
+# Date/time:                    2011-03-07 20:15:10
 # --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-# Dumping structure for table sctms-dev.cms_blogentry
-DROP TABLE IF EXISTS `cms_blogentry`;
-CREATE TABLE IF NOT EXISTS `cms_blogentry` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `author_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `slug` varchar(50) NOT NULL,
-  `tags` varchar(150) NOT NULL,
-  `hits` int(11) NOT NULL,
-  `up` int(11) NOT NULL,
-  `down` int(11) NOT NULL,
-  `text` longtext NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `author_id` (`author_id`),
-  KEY `cms_blogentry_42dc49bc` (`category_id`),
-  KEY `cms_blogentry_56ae2a2a` (`slug`),
-  CONSTRAINT `category_id_refs_id_4902694e` FOREIGN KEY (`category_id`) REFERENCES `cms_category` (`id`),
-  CONSTRAINT `author_id_refs_id_43c89379` FOREIGN KEY (`author_id`) REFERENCES `auth_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
 # Dumping data for table sctms-dev.cms_blogentry: ~2 rows (approximately)
 /*!40000 ALTER TABLE `cms_blogentry` DISABLE KEYS */;
 INSERT INTO `cms_blogentry` (`id`, `author_id`, `category_id`, `date`, `title`, `slug`, `tags`, `hits`, `up`, `down`, `text`) VALUES
@@ -40,40 +17,12 @@ INSERT INTO `cms_blogentry` (`id`, `author_id`, `category_id`, `date`, `title`, 
 	(2, 8, 2, '2011-03-07 19:44:50', 'Jak hoří les?', 'jak-hori-les', 'les, ohen, otazky, odpovedi', 3, 0, 0, 'Používáte-li nástroj pro čtení obrazovky, vypněte Dynamické vyhledávání Google kliknutím sem.\r\nGoogle\r\n\r\n	×\r\n\r\nDynamické vyhledávání je zapnuto ▼\r\n\r\nWeb Obrázky Videa Mapy Zprávy Překladač Gmail další ▼\r\ntheNSLcz@gmail.com | Webová historie | Nastavení ▼ | Odhlásit se\r\nRozšířené vyhledávání\r\nPřibližný počet výsledků: 2 040 000 (0,34 s) \r\nVýsledky hledání\r\nPython Database Programming - 15:57 - [ Přeložit tuto stránku ]\r\n10 Nov 2010 ... Talk video and slides: Using the Python Database API ... SQL Relay is a persistent database connection pooling, proxying and load balancing ...\r\nwiki.python.org/moin/DatabaseProgramming/ - Archiv - Podobné\r\n►\r\nUsingDbApiWithPostgres - PythonInfo Wiki - [ Přeložit tuto stránku ]\r\n15 Feb 2010 ... import psycopg2 as dbapi2 db = dbapi2.connect (database ...\r\nwiki.python.org/moin/UsingDbApiWithPostgres - Archiv - Podobné\r\nZobrazit další výsledky z webu python.org\r\nProgramming Python - Python Database Programming - Connecting to ... - 15:57 - [ Přeložit tuto stránku ]\r\nThese tutorials explore the various Python modules available for database administration. Particular emphasis will be laid on MySQL and PostgreSQL database ...\r\npython.about.com/.../pythonanddatabases/Connecting_to_Databases_With_Python.htm - Archiv - Podobné\r\nConnect to Database Using Python « Code Ghar - [ Přeložit tuto stránku ]\r\n8 Dec 2007 ... Although you can connect to databases using Python on many platforms, ... Skills Expected of a Linux Admin · Software in the Public Interest ...\r\ncodeghar.wordpress.com/.../connect-to-database-using-python/ - Archiv - Podobné\r\nBuilding a web-admin around an existing MS SQL table (python ... - [ Přeložit tuto stránku ]\r\nBuilding a web-admin around an existing MS SQL table (python/django problem? ... Connecting to MS SQL Server using python on linux with \'Windows Credentials ...\r\nstackoverflow.com/.../building-a-web-admin-around-an-existing-ms-sql-table-python-django-problem - Archiv\r\nDjango | Databases | Django documentation - [ Přeložit tuto stránku ]\r\n13 Oct 2010 ... In order for the python manage.py syncdb command to work, your Oracle database ... CONNECT WITH ADMIN OPTION; RESOURCE WITH ADMIN OPTION .... Connecting to the database; Creating your tables; Notes on specific fields ...\r\ndocs.djangoproject.com/en/dev/ref/databases/ - Archiv - Podobné\r\nPython for the SQL Server DBA - [ Přeložit tuto stránku ]\r\nCelkem příspěvků: 12 - Počet autorů: 10 - Poslední příspěvek: 27. srpen 2009\r\nSQL Home > Database Administration > Python for the SQL Server DBA ... This is what I primarily use it for in connection with SQL. ... into a SQL Temp Table line by line and then parse it using pure T-SQL it is generally ...\r\nwww.simple-talk.com › ... › Database Administration - Archiv - Podobné\r\nPython for Unix and Linux system administration - Výsledky hledání v Google Books\r\nNoah Gift, Jeremy M. Jones - 2008 - Computers - 433 str.\r\nTables connected with a foreign key relationship can even be accessed as an attribute of ... Storm is a relative newcomer to the database arena for Python, ...\r\nbooks.google.cz/books?isbn=0596515820...\r\n[Python] chCounter <= 3.1.3 SQL Injection Vulnerability - [ Přeložit tuto stránku ]\r\n1 příspěvek - 1 autor - Poslední příspěvek: 19. listopad 2010\r\nAccess to administration site(can be bypassed if magic_quotes off) ... -1: print \'[-] Could not acces table. Acces denied. ...\r\nwww.hackxcrack.es/.../4125-python-chcounter-3-1-3-sql-injection-vulnerability.html?... - Archiv\r\nFree win32 appwizard Python download - Python win32 appwizard ... - [ Přeložit tuto stránku ]\r\nPython. Details · Download. Odbc connection on win32 script implement the features of retrieving dsn and tables list on win32. ...\r\nwww.lastbeatniks.tk/free-win32-appwizard/python/ - Archiv\r\nVše\r\nObrázky\r\nVidea\r\nZprávy\r\nKnihy\r\nVíce\r\nPraha\r\nZměnit místo\r\nMožnosti vyhledávání\r\nProhledat web\r\nStránky pouze česky\r\nPřeložené cizojazyčné stránky\r\nVíce nástrojů\r\n1	\r\n2\r\n3\r\n4\r\n5\r\n6\r\n7\r\n8\r\n9\r\n10\r\nDalší\r\nTipy pro vyhledávání Sdělte nám svůj názor\r\nGoogle HomeInzerujte s GooglemOsobní údajeVše o Googlu');
 /*!40000 ALTER TABLE `cms_blogentry` ENABLE KEYS */;
 
-
-# Dumping structure for table sctms-dev.cms_category
-DROP TABLE IF EXISTS `cms_category`;
-CREATE TABLE IF NOT EXISTS `cms_category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
 # Dumping data for table sctms-dev.cms_category: ~2 rows (approximately)
 /*!40000 ALTER TABLE `cms_category` DISABLE KEYS */;
 INSERT INTO `cms_category` (`id`, `name`) VALUES
 	(1, 'pes'),
 	(2, 'les');
 /*!40000 ALTER TABLE `cms_category` ENABLE KEYS */;
-
-
-# Dumping structure for table sctms-dev.cms_comment
-DROP TABLE IF EXISTS `cms_comment`;
-CREATE TABLE IF NOT EXISTS `cms_comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `author_id` int(11) NOT NULL,
-  `topic_id` int(11) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `date` datetime NOT NULL,
-  `up` int(11) NOT NULL,
-  `down` int(11) NOT NULL,
-  `text` longtext NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `author_id` (`author_id`),
-  KEY `cms_comment_57732028` (`topic_id`),
-  CONSTRAINT `author_id_refs_id_154d9dca` FOREIGN KEY (`author_id`) REFERENCES `auth_user` (`id`),
-  CONSTRAINT `topic_id_refs_id_57d1b138` FOREIGN KEY (`topic_id`) REFERENCES `cms_blogentry` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 # Dumping data for table sctms-dev.cms_comment: ~0 rows (approximately)
 /*!40000 ALTER TABLE `cms_comment` DISABLE KEYS */;
